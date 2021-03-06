0f9720338b23e6c32fa91a5a6f3c75908ffaf0c3
Pedro Campos <pedro@cdias.dev>
2021-03-05 23:13:27 -0300

    Beautify Code

--- main.c
+++ main.c
@@ -1,43 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
+%:include <stdio.h>
+%:include <stdlib.h>
+%:include <stdbool.h>
 
-typedef struct {
+typedef struct <%
     double v;
     unsigned i;
-} key;
+%> key;
 
-key max(double *a, unsigned l) {
+key max(double *a, unsigned l) <%
     double m = 0;
     unsigned j = 0;
     for(unsigned i = 0; i < l; i++) 
-        if(a[i] > m) {
-            m = a[i];
+        if(a<:i:> > m) <%
+            m = a<:i:>;
             j = i;
-        }
-    return (key){m, j}; 
-}
+        %>
+    return (key)<%m, j%>; 
+%>
 
-double sum(double *a, unsigned l) {
+double sum(double *a, unsigned l) <%
     double m = 0;
     for(unsigned i = 0; i < l; i++) 
-        m += a[i];
+        m += a<:i:>;
     return m;
-}
+%>
 
-bool has(unsigned *a, unsigned l, unsigned t) {
+bool has(unsigned *a, unsigned l, unsigned t) <%
     for(unsigned i = 0; i < l; i++)
-        if(a[i] == t) return true;
+        if(a<:i:> == t) return true;
     return false;
-}
+%>
 
-int main() {
-    double Z[1000];
+int main() <%
+    double Z<:1000:>;
     unsigned G = 0;
 
     unsigned N = 1;
     double H = 1;
-    while(1) {
+    while(1) <%
         scanf("%u %lf", &N, &H);
         if(!N && !H) break;
         double *a = malloc(sizeof(double) * N);
@@ -46,36 +46,34 @@ int main() {
         for(unsigned i = 0; i < N; i++) 
             scanf("%lf", a + i);
         double s = sum(a, N);
-        if(s == H || !s) Z[G++] = 0;
-        else if(s < H) Z[G++] = -1;
-        else {
-            do {
+        if(s == H || !s) Z<:G++:> = 0;
+        else if(s < H) Z<:G++:> = -1;
+        else <%
+            do <%
+                if(c >= N) break;
                 double pM = 0.0;
                 key k;
-                if(!c) {
+                if(!c) <%
                     k = max(a, N);
-                    b[c++] = k.i;
-                    a[k.i] = k.v - H;
-                }
+                    b<:c++:> = k.i;
+                    a<:k.i:> = k.v - H;
+                %>
                 for(unsigned l = 0; l < c; l++)
-                    pM += a[b[l]];
+                    pM += a<:b<:l:>:>;
                 k = max(a, N);
-                b[c++] = k.i;
+                b<:c++:> = k.i;
                 double m = (k.v + pM) / c;
-                printf("m: %lf, c: %u\n", m, c);
                 for(unsigned l = 0; l < c; l++)
-                    a[b[l]] = m;
-                printf("%s, max.v: %lf, max.i: %u\n", has(b, c, max(a, N).i) ? "true" : "false", max(a, N).v, max(a, N).i);
-                printf("%s, s: %lf, sum: %lf, H: %lf\n", s - sum(a, N) != H ? "true" : "false", s, sum(a, N), H);
-            } while(s - sum(a, N) != H || !has(b, c > N ? N : c, max(a, N).i));
-            Z[G++] = max(a, N).v;    
-        }
+                    a<:b<:l:>:> = m;
+            %> while(s - sum(a, N) != H || !has(b, c, max(a, N).i));
+            Z<:G++:> = max(a, N).v;    
+        %>
         free(a);
         free(b);
-    }
-    for(unsigned i = 0; i < G; i++) {
-        if(!Z[i]) printf(":D\n");
-        else if(Z[i] == -1) printf("-.-\n");
-        else printf("%.4f\n", Z[i]);
-    }
-}
+    %>
+    for(unsigned i = 0; i < G; i++) <%
+        if(!Z<:i:>) printf(":D\n");
+        else if(Z<:i:> == -1) printf("-.-\n");
+        else printf("%.4f\n", Z<:i:>);
+    %>
+%>
